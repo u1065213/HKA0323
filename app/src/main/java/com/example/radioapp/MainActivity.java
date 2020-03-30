38d6eacc98fc255e5ba8d60d7368fd68c8d859e5
unknown <u1065213@hk.edu.tw>
2020-03-30 10:52:28 +0800

    20200330

--- app/src/main/java/com/example/radioapp/MainActivity.java
+++ app/src/main/java/com/example/radioapp/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.radioapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnOK(View view) {
+        TextView txvLike = findViewById(R.id.textView2);
+        RadioGroup kGrilGruop = findViewById(R.id.kGrilGroup);
+
+        int id =kGrilGruop.getCheckedRadioButtonId();
+        RadioButton selected =findViewById(id);
+
+        txvLike.setText("答案是:"+selected.getText());
     }
 }
